c550d65f34728857b16fe619ee38bf770cface21
Josué Lobo <josuevitorp.lobo@gmail.com>
2024-10-17 14:17:57 -0300

    register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -76,7 +76,7 @@ export function Register() {
 		}
 	};
 	return (
-		<div className="flex items-center justify-center min-h-screen bg-gray-100">
+		<div className="flex items-center justify-center min-h-screen">
 			<form onSubmit={handleSubmit}>
 				<Card className="w-[400px]">
 					<CardHeader>
